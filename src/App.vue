cb4028a0c8f7b9bb1e80c514e4dd409d53b7de79
AbstractProgrammer <AbstractProgrammer@example.com>
2024-01-25 19:28:15 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <app-nav></app-nav>
+  </div>
+</template>
+
+<script>
+import navbar from './components/navbar.vue'
+export default {
+  components: {
+    'app-nav': navbar
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
